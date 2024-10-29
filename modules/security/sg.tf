c2bf4d12f5d659cf630f59064a2b2580ee099311
Ubuntu <ubuntu@ip-10-20-1-165.ap-east-1.compute.internal>
2024-10-29 09:02:51 +0000

    change the security module configuration

--- modules/security/sg.tf
+++ modules/security/sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "internet_wlz_sg" {
+resource "aws_security_group" "internet_sg" {
   name   = "internet_access_sg"
   vpc_id = var.vpc_id
 
@@ -10,83 +10,68 @@ resource "aws_security_group" "internet_wlz_sg" {
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "47"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "50"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 10022
-    to_port     = 10022
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4500
-    to_port     = 4500
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4784
-    to_port     = 4784
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4789
-    to_port     = 4789
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 500
-    to_port     = 500
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 50
-    to_port     = 51
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 50
-    to_port     = 51
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tpc"
+    cidr_blocks = ["223.119.0.0/16"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["18.163.189.237/32"]
+  }
+
+  ingress {
+    from_port   = 123446
+    to_port     = 13046
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
+  tags = {
+    Name = "internet-sg"
+  }
+}
+
+resource "aws_security_group" "mpls_sg" {
+  name   = "mpls_access_sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tpc"
+    cidr_blocks = ["172.16.0.0/16"]
+  }
+
+  tags = {
+    Name = "mpls-sg"
+  }
+}
+
+resource "aws_security_group" "lan_sg" {
+  count = var.lan_sg_enable == true ? 1 : 0
+
+  name   = "lan_access_sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "wlz-internet-sg"
+    Name = "lan-sg"
   }
 }
