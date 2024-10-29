0fbebaaf909e3b3acbd89b0eecc23dbfe0d901f9
Ubuntu <ubuntu@ip-10-20-1-165.ap-east-1.compute.internal>
2024-10-29 14:32:28 +0000

    change the s3 key to state/compute/terraform.tfstate

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "cmi-sdw-tfstate"
-    key            = "state/terraform.tfstate"
+    key            = "state/compute/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-state-lockid"
     encrypt        = true
