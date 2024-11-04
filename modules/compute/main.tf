7b8a02dd4696e6d49a28f5836a7ad22d3f9fb3b7
CMI-Product <willisxiongwei@cmi.chinamobile.com>
2024-11-04 09:29:04 +0000

    delete source_dest_check in compute module and change the backend to terraform cloud

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "main" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  source_dest_check = false
   user_data     = file(var.user_data_file)
 
   network_interface {
